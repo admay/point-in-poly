5f621ee550a0a68794aeb36ff16a1bc2584856b5
Michael Zavarella <mzavarella30@gmail.com>
2024-07-01 11:40:27 -0400

    Create the initial library

---
+++ src/admay/point_in_poly.clj
@@ -0,0 +1,42 @@
+(ns admay.point-in-poly)
+
+(defn in?
+  "contains? has some weird behavior. Returns true if the coll contains the elm."
+  [coll elm]
+  (some #(= elm %) coll))
+  
+(defn colinear?
+  "Check to see if a value p is at or beyond the lower bound but
+  does not exceed the upper bound."
+  [p x y]
+  (or (and (<= x p) (> y p))
+      (and (> x p) (<= y p))))
+
+(defn crosses?
+  "Checks to see if a ray from point p will intersect an edge from point (x1,y1) to point (x2,y2)."
+  [[px py] [[x1 y1] [x2 y2]]]
+  (let [vt (/ (- py y1) (- y2 y1))]
+    (< px (+ x1 (* vt (- x2 x1))))))
+
+(defn num-crosses
+  "Returns 1 for an intersection with a ray from point p to the edge and 0 for a lack thereof"
+  [point edge]
+  (let [[_ py] point
+        [[_ y1] [_ y2]] edge]
+    (if (colinear? py y1 y2)
+      (if (crosses? point edge)
+        1
+        0)
+      0)))
+
+(defn count-crosses
+  "Counts the number of times a ray from point p will intersect the edges of a polygon."
+  [point polygon]
+  (reduce + (for [n (range (dec (count polygon)))]
+              (num-crosses point [(nth polygon n)
+                                  (nth polygon (inc n))]))))
+
+(defn inside-polygon?
+  "Determines whether or not a point is in a polygon."
+  [point polygon]
+  (odd? (count-crosses point polygon)))
---
+++ test/admay/point_in_poly_test.clj
@@ -0,0 +1,3 @@
+(ns admay.point-in-poly-test
+  (:require [clojure.test :refer :all]
+            [admay.point-in-poly :refer :all]))
